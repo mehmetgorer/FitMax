fb593ffac2457c89c16cf34ddd9069161211a06d
Mehmet Görer <mehmetgorer@gmail.com>
2026-04-30 04:37:48 +0300

    Initial commit

---
+++ FitMax.Entity/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitMax.Entity.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductType { get; set; }
+        public string Description { get; set; }
+        public decimal Purchaseprice { get; set; }
+        public decimal Price { get; set; }
+        public bool Status { get; set; }
+        public int Stock { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
